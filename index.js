39e94d9feb23ea3a10beac45cf5012db613e01a3
Ishan Bhattacharya <generatedelectricity@gmail.com>
2024-11-01 13:45:04 +0530

    check

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:5173", // React app URL
+        origin: "https://chess-frontend-neon.vercel.app/", // React app URL
         methods: ["GET", "POST"]
     }
 });
